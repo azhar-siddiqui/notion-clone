d2a607fd3af60053c79cde45f09eac240fb293f0
Azhar Sayyed <azhartsiddiqui@gmail.com>
2023-10-14 15:42:03 +0530

    dark mood and light mood added

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+import { Logo } from "./logo";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center w-full p-6  bg-background z-50 dark:bg-[#1f1f1f]">
+      <Logo />
+      <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+        <Button variant="ghost">Privacy Policy</Button>
+        <Button variant="ghost">Terms & Condition</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
